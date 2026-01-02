a52b113a854395f2464aa2bbe048823f75355a2c
DiegoMendoz <daygon2001@gmail.com>
2026-01-01 23:35:30 -0600

    se modifico index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './APP/App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
+root.render(<App />);
